8c50c36f7911f8e9f7136728aa1c071f64b5da8e
igor bastos <igorbastoscic@gmail.com>
2022-06-23 14:46:28 -0300

    Busca por id Adicionado

--- src/main/java/com/example/parkingcontrol/controlles/ParkingSpotController.java
+++ src/main/java/com/example/parkingcontrol/controlles/ParkingSpotController.java
@@ -13,6 +13,8 @@ import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -47,6 +49,14 @@ public class ParkingSpotController {
     public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpot(){
         return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
 
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if(!parkingSpotModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not Found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
     }
 
 }
